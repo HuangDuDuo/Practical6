7c32bebec8f89957afd76b4226857556f51fdd9f
Apple <huangduduo@hotmail.com>
2025-03-25 16:02:52 +0800

    SIR

---
+++ SIR.py
@@ -0,0 +1,28 @@
+# import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+N,I,S,R = 10000,1,9999,0
+beta,gamma = 0.3,0.05
+Infected = []
+Susceptible = []
+Recovered = []
+for i in range (1001):
+    Infected.append(I)
+    Susceptible.append(S)
+    Recovered.append(R)
+    n1 = np.random.choice(range(2),S,p=[1-beta,beta])
+    In = np.sum(n1)
+    n2 = np.random.choice(range(2),I,p=[1-gamma,gamma])
+    Re = np.sum(n2)
+    R = R+Re
+    I = I+In-Re
+    S = S-In
+plt.plot(Susceptible,label='Susceptible')
+plt.plot(Infected,label='Infected')
+plt.plot(Recovered,label='Recovered')
+plt.title('SIR model')
+plt.xlabel('time')
+plt.ylabel('number of people')
+plt.legend()
+plt.show()
+
